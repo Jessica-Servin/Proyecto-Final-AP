bc0a3e83fe0226a30d99770552022dc580c81726
Jessica-Servin <jessycaservyn@hotmail.com>
2022-10-31 20:05:17 -0300

    Complementos y Login Agregados

---
+++ ProyectoFinalAP/BackEnd/Jessica/src/main/java/com/portfolio/Jessica/JessicaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Jessica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JessicaApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinalAP/BackEnd/Jessica/src/test/java/com/portfolio/Jessica/JessicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Jessica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JessicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
